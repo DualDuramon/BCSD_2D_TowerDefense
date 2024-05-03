649778b92bd0587a5c424474dd270f287cce7749
DualDura <jdsan216@gmail.com>
2024-05-03 23:25:01 +0900

    Make BuffTower

--- Assets/Scripts/TowerDataViewer.cs
+++ Assets/Scripts/TowerDataViewer.cs
@@ -4,24 +4,18 @@ using TMPro;
 
 public class TowerDataViewer : MonoBehaviour
 {
-    [SerializeField]
-    private Image               imageTower;
-    [SerializeField]
-    private TextMeshProUGUI     textDamage;
-    [SerializeField]
-    private TextMeshProUGUI     textRate;
-    [SerializeField]
-    private TextMeshProUGUI     textRange;
-    [SerializeField]
-    private TextMeshProUGUI     textLevel;
-    [SerializeField]
-    private TowerAttackRange    towerAttackRange;
-    [SerializeField]
-    private Button              buttonUpgrade;
-    [SerializeField]
-    private SystemTextViewer    systemTextViewer;
+    [SerializeField] private Image              imageTower;
+    [SerializeField] private TextMeshProUGUI    textDamage;
+    [SerializeField] private TextMeshProUGUI    textRate;
+    [SerializeField] private TextMeshProUGUI    textRange;
+    [SerializeField] private TextMeshProUGUI    textLevel;
+    [SerializeField] private TextMeshProUGUI    textUpgradeCost;
+    [SerializeField] private TextMeshProUGUI    textSellCost;
 
-    private TowerWeapon         currentTower;
+    [SerializeField] private TowerAttackRange   towerAttackRange;
+    [SerializeField] private Button             buttonUpgrade;
+    [SerializeField] private SystemTextViewer   systemTextViewer;
+                     private TowerWeapon        currentTower;
 
     private void Awake() {
         OffPanel();
@@ -48,17 +42,26 @@ public class TowerDataViewer : MonoBehaviour
 
         if (currentTower.WeaponType == WeaponType.Cannon || currentTower.WeaponType == WeaponType.Laser) {
             imageTower.rectTransform.sizeDelta = new Vector2(88, 59);
-            textDamage.text = "Damage : " + currentTower.Damage;
+            textDamage.text = "Damage : " + currentTower.Damage
+                                + "+" + "<color=red>" + currentTower.AddedDamage.ToString("F1") + "</color>";
         }
         else {
             imageTower.rectTransform.sizeDelta = new Vector2(59, 59);
-            textDamage.text = "Slow : " + currentTower.Slow * 100 + "%";
+
+            if (currentTower.WeaponType == WeaponType.Slow) {
+                textDamage.text = "Slow : " + currentTower.Slow * 100 + "%";
+            }
+            else if (currentTower.WeaponType == WeaponType.Buff) {
+                textDamage.text = "Buff : " + currentTower.Buff * 100 + "%";
+            }
         }
 
         imageTower.sprite = currentTower.TowerSprite;
         textRate.text = "Rate : " + currentTower.Rate;
         textRange.text = "Range : " + currentTower.Range;
         textLevel.text = "Level : " + currentTower.Level;
+        textUpgradeCost.text = currentTower.UpgradeCost.ToString();
+        textSellCost.text = currentTower.SellCost.ToString();
 
         buttonUpgrade.interactable = currentTower.Level < currentTower.MaxLevel ? true : false;     //타워가 만랩이라 업그레이드 불가시 버튼 비활성화
     }
